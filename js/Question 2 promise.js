23b12e98c82462572a1f79e954d5cacf782cfcab
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2025-02-24 20:35:07 +0200

    Question 2

---
+++ js/Question 2 promise.js
@@ -0,0 +1,18 @@
+/* b)
+The main differences between callbacks and promises is:
+Syntax: Callbacks are function-based, whereas promises have methods like .then() and .catch()
+
+Promises solve:
+Callback hell: Promises provide a flat chainable structure that prevents nesting.
+Error handling: Promises has .catch() that automates error handling.*/
+
+
+function fetchOrderStatus(orderId) {
+    return new Promise((resolve, reject) => {
+            if (orderId >= 1000 && orderId <= 5000) {
+                resolve("Processed");
+            } else {
+                reject("Error: Invalid orderId. Please provide an orderId between 1000 and 5000.");
+            }
+    });
+}
