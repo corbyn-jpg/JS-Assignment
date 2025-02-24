9b3f944d0a444e8b19ddbb35a4af225001760ca2
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2025-02-24 19:30:07 +0200

    Update Question 1 callback.js

--- js/Question 1 callback.js
+++ js/Question 1 callback.js
@@ -1,1 +1,5 @@
+/* a)
+A callback function is a function that is passed as an argument into another function. 
+Callbacks are used in asynchronous programming because they pass a function that is then executed later. 
+A real world example is in API requests, specifically when the data is called.*/
 
